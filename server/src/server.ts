741a4ce5cbfd8b74ba610c8349e39f5655eaecbc
Aditya Mehra <aditya97mehra@gmail.com>
2021-05-17 22:03:23 +0530

    [connected] server to db 🎡

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
+// static imports
 import { ApolloServer } from 'apollo-server'
 import { createContext } from './context'
 import { schema } from './schema'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const server = new ApolloServer({
   schema,
@@ -11,6 +15,6 @@ server.listen().then(({ url }) =>
   console.log(
     `\
 🚀 Server ready at: ${url}
-⭐️ See sample queries: http://pris.ly/e/ts/graphql-auth#using-the-graphql-api`,
+⭐️ Hello Graphql`,
   ),
 )
